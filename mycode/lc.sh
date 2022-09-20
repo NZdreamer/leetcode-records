6bcb35371bcad39173c0506725db484c8cc060aa
NZdreamer <hi45288871@163.com>
2022-09-20 15:34:37 -0700

    notes

--- mycode/lc.sh
+++ mycode/lc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-alias lc="function _lc() { cd /D/USC/leetcode/mycode && mkdir "$1" && cd "$1" && touch Solution.java && code Solution.java; }; _lc"
+alias lc="function _lc() { cd /E/USC/leetcode/mycode && mkdir "$1" && cd "$1" && touch Solution.java && code Solution.java; }; _lc"
